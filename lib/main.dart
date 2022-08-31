d8f00310722bea6d706fb8eec2e3519496221910
suyogkokaje <suyogkokaje6@gmail.com>
2022-08-31 17:48:34 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'app/data/services/storage/services.dart';
+import 'app/modules/home/view.dart';
+import 'package:get_storage/get_storage.dart';
+void main() async{
+   await GetStorage.init();
+   await Get.putAsync(() => StorageService().init());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'TO DO',
+      home: HomePage(),
+    );
+  }
+}
+
